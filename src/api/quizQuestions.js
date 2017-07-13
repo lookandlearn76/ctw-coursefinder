f6222cf7561256663254c44213d2da15f3983802
Mark <lookandlearn76@yahoo.co.uk>
2017-07-13 15:52:33 +0100

    initial commit

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,89 @@
+var quizQuestions = [
+  {
+      question: "Which of these options do you consider to be of highest priority for your church to understand?",
+      answers: [
+          {
+              content: "Spiritual Growth",
+              type: "Discipleship"
+          },
+          {
+              content: "The Bible",
+              type: "The Bible"
+          },
+          {
+              content: "Love and Identity",
+              type: "Love and Identity"
+          }
+      ]
+  },
+  {
+      question: "Choose also from the next set of options for your church needs?",
+      answers: [
+          {
+              content: "Love and Identity",
+              type: "Love and Identity"
+          },
+          {
+              content: "Evangelism",
+              type: "Evangelism"
+          },
+          {
+              content: "Discipleship",
+              type: "Discipleship"
+          }
+      ]
+  },
+  {
+      question: "Choose again from the following set of options",
+      answers: [
+          {
+              content: "Leadership",
+              type: "Leadership"
+          },
+          {
+              content: "Prayer",
+              type: "Prayer"
+          },
+          {
+              content: "Discipleship",
+              type: "Discipleship"
+          }
+      ]
+  },
+  {
+      question: "What else is most important to you as a Pastor?",
+      answers: [
+          {
+              content: "Minstry",
+              type: "Ministry"
+          },
+          {
+              content: "Love and Identity",
+              type: "Love and Identity"
+          },
+          {
+              content: "Worship",
+              type: "Worship"
+          }
+      ]
+  },
+  {
+      question: "What else from the list below would you like to consider improving in your church?",
+      answers: [
+          {
+              content: "Worship",
+              type: "Worship"
+          },
+          {
+              content: "Discipleship",
+              type: "Discipleship"
+          },
+          {
+              content: "Missions",
+              type: "Missions"
+          }
+      ]
+  }
+];
+
+export default quizQuestions;
---
+++ src/components/Result.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+
+ function Result(props) {
+   return (
+    <ReactCSSTransitionGroup
+     className="container result"
+     component="div"
+     transitionName="fade"
+     transitionEnterTimeout={800}
+     transitionLeaveTimeout={500}
+     transitionAppear
+     transitionAppearTimeout={500}
+    >
+      <div>
+         The right course for your church would be <strong>{props.quizResult}</strong>!
+      </div>
+    </ReactCSSTransitionGroup>
+   );
+ }
+
+ Result.propTypes = {
+   quizResult: PropTypes.string.isRequired,
+ };
+
+ export default Result;
